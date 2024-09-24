2f61c0018ea0e50f007ada66d4d935701c080431
IVAN0137 <ivanmartinezgudino55@gmail.com>
2024-09-24 16:20:50 -0600

    se genero un cambion en el php y el boton de cerrar sesion

--- logout.php
+++ logout.php
@@ -1,6 +1,13 @@
 <?php
-session_start();
-session_destroy();
-header("Location: login.html");
-?>
+session_start(); // Inicia la sesión
 
+// Elimina todas las variables de sesión
+session_unset();
+
+// Destruye la sesión
+session_destroy();
+
+// Redirige al usuario a la página principal o de inicio de sesión
+header("Location: index.html");
+exit(); // Asegúrate de detener la ejecución del script
+?>
